820386a2f54b1bf961aaccb601443bdd628a5493
prakritibhatt <prakritibhatt578@gmail.com>
2026-03-08 15:31:15 +0530

    Redux

--- 11_reduxToolKitTodo/src/main.jsx
+++ 11_reduxToolKitTodo/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Provider } from 'react-redux'
+import { store } from './app/store.js'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  //store is a property and the value passed as store is the one we created in store.js
+  <Provider store={store}> 
     <App />
-  </StrictMode>,
+  </Provider>,
 )
